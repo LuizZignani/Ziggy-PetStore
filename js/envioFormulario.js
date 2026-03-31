f380c8eb3bec582c4c082e21d83bc900339eec75
LuizZignani <83853587+LuizZignani@users.noreply.github.com>
2026-03-30 22:20:12 -0300

    Delete js/envioFormulario.js
    Não ira ser mais usado.

--- js/envioFormulario.js
+++
@@ -1,4 +0,0 @@
-function enviarFormularioParaEmailCadastrado() {
-    getEmail = document.getElementsByClassName("class_input_email")[0].value;
-    document.getElementById("FormularioCadastro").action = "https://formsubmit.co/" + getEmail;
-}
