3a01f8b2087cf2ec69518fb7e72e185a80e1aade
LuizZignani <83853587+LuizZignani@users.noreply.github.com>
2026-03-30 22:18:03 -0300

    Formulário + atualizações
    Criação do formulário em servicos.html, criação de um arquivo js para dar o efeito de formulário sendo enviado.
    Atualização do código CSS auxiliar das páginas, e organização do código com a adição de mais divs.

---
+++ js/enviarFormulario.js
@@ -0,0 +1,5 @@
+function enviarFormulario() {
+    alert('Serviço agendado!');
+    document.getElementById("paginaFormulario").style.display = "none";
+    document.getElementById("textoFormulario1").style.display = "block";
+}
